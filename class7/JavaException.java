55ac09dfad9b6b15a5dbf3727050b86b7b5d9916
Enoch <walkerhaozheng@gmail.com>
2020-01-25 11:02:04 -0800

    Updated code for class 7

--- class7/JavaException.java
+++ class7/JavaException.java
@@ -1,8 +1,19 @@
 import java.io.FileWriter;
 public class JavaException {
+
+
     public static void main(String[] args) {
-        checkAge(17);
+        try {
+            System.out.println(0 / 1);
+            int[] array = new int[]{1, 2};
+            System.out.println(array[3]);
+        } catch (Exception e) {
+            System.out.println("An exception occurs.");
+        } finally {
+            System.out.println("After try...catch block");
+        }
     }
+
     public static void checkAge(int age) {
         if (age < 18) {
             throw new ArithmeticException("Access denied: You must be at least 18 years old.");
